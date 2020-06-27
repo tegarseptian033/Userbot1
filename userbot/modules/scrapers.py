edb3db9409b7f97c54a4d68a21db91a1f812cc28
luqmanvps <67447668+luqmanvps@users.noreply.github.com>
2020-06-27 19:26:25 +0700

    Transtale .Carbon to Indonesia

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -116,8 +116,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        caption="Dibuat dengan [Carbon](https://carbon.now.sh/about/),\
+        \na Bot Mod by [LuqmanBod](https://t.me/luqmanbod)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
